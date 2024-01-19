f9eee9d7740ba9acea8bdf6bacd6ea9b49e080e6
henrychris <henrychris2003@gmail.com>
2024-01-19 17:39:15 +0100

    implement icurrentUser

--- src/TrainAPI.Application/Interfaces/ICurrentUser.cs
+++ src/TrainAPI.Application/Interfaces/ICurrentUser.cs
@@ -2,5 +2,7 @@ namespace TrainAPI.Application.Interfaces;
 
 public interface ICurrentUser
 {
-
+    string? UserId { get; }
+    string? Email { get; }
+    string? Role { get; }
 }
---
+++ src/TrainAPI.Infrastructure/Services/CurrentUser.cs
@@ -0,0 +1,16 @@
+using TrainAPI.Application.Interfaces;
+using TrainAPI.Domain.Constants;
+
+namespace TrainAPI.Infrastructure.Services;
+
+public class CurrentUser(IHttpContextAccessor httpContextAccessor) : ICurrentUser
+{
+    public string? UserId =>
+        httpContextAccessor.HttpContext?.User.FindFirst(JwtClaims.USER_ID)?.Value;
+
+    public string? Email =>
+        httpContextAccessor.HttpContext?.User.FindFirst(JwtClaims.EMAIL)?.Value;
+
+    public string? Role =>
+        httpContextAccessor.HttpContext?.User.FindFirst(JwtClaims.ROLE)?.Value;
+}
