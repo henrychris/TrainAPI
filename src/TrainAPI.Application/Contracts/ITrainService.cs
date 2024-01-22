1f28a935d4c24def2d79ad6c3d1c4f29abb01f53
henrychris <henrychris2003@gmail.com>
2024-01-22 14:36:02 +0100

    edit param spelling

--- src/TrainAPI.Application/Contracts/ITrainService.cs
+++ src/TrainAPI.Application/Contracts/ITrainService.cs
@@ -5,7 +5,7 @@ namespace TrainAPI.Application.Contracts;
 public interface ITrainService
 {
     Task CreateTrain(Train train);
-    Task<Train?> GetTrain(string requestTrainId);
+    Task<Train?> GetTrain(string trainId);
     Task UpdateTrain(Train train);
     Task DeleteTrain(Train train);
     IQueryable<Train> GetQueryable();
